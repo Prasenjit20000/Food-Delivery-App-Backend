6f5a0e262773440e4997786779cf43e13b459b35
Prasenjit20000 <prasend1008@gmail.com>
2025-03-29 11:10:03 +0530

    Removed all existing files

--- backend/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const  connectDB = async () =>{
-    const connectionString=process.env.DB_CONNECTION_STRING;
-    await mongoose.connect(connectionString).then(()=>console.log("DB Connected"));
-}
